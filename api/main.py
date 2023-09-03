c1010d4a5793ee0058268ce95ab19afd87b7f97f
Luis Pérez <luis.perez@protonmail.com>
2023-09-03 11:03:32 -0600

    agregando soporte para cors

--- api/main.py
+++ api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
-from typing import Annotated
+from typing import Annotated, List
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import Rol, Grado, Usuario, Docente, Tarea
 from schemas import (
@@ -14,6 +14,22 @@ from schemas import (
 )
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:4200",
+    "http://192.168.111.18:8000",
+    "http://192.168.111.7:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
@@ -29,14 +45,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+@app.post("/tareas", status_code=status.HTTP_201_CREATED)
 async def create_post(task: TareaBase, db: db_dependency):
     db_task = Tarea(**task.model_dump())
     db.add(db_task)
     db.commit()
 
 
-@app.get("/task/{task_id}", status_code=status.HTTP_200_OK)
+@app.get("/tarea/{task_id}", status_code=status.HTTP_200_OK)
 async def read_post(task_id: int, db: db_dependency):
     task = db.query(Tarea).filter(Tarea.id == task_id).first()
     if task is None:
@@ -46,14 +62,14 @@ async def read_post(task_id: int, db: db_dependency):
     return task
 
 
-@app.post("/users", status_code=status.HTTP_201_CREATED)
+@app.post("/usuarios", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UsuarioBase, db: db_dependency):
     db_user = Usuario(**user.model_dump())
     db.add(db_user)
     db.commit()
 
 
-@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+@app.get("/usuarios/{user_id}", status_code=status.HTTP_200_OK)
 async def read_user(user_id: int, db: db_dependency):
     user = db.query(Usuario).filter(Usuario.id == user_id).first()
     if user is None:
@@ -61,3 +77,22 @@ async def read_user(user_id: int, db: db_dependency):
             status_code=status.HTTP_404_NOT_FOUND, detail="Usuario not found"
         )
     return user
+
+
+@app.get("/usuarios", status_code=status.HTTP_200_OK)
+async def get_users(db: db_dependency, skip: int = 0, limit: int = 100):
+    users = db.query(Usuario).offset(skip).limit(limit).all()
+    return users
+
+
+@app.post("/roles", status_code=status.HTTP_201_CREATED)
+async def create_role(role: RolBase, db: db_dependency):
+    db_role = Rol(**role.model_dump())
+    db.add(db_role)
+    db.commit()
+
+
+@app.get("/roles", status_code=status.HTTP_200_OK)
+async def get_roles(db: db_dependency, skip: int = 0, limit: int = 100):
+    roles = db.query(Rol).offset(skip).limit(limit).all()
+    return roles
