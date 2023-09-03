b2e21332d42709cf3aaa0aa2259871f20de1479c
Luis Pérez <luis.perez@protonmail.com>
2023-09-03 14:52:53 -0600

    implementando toolbar y primer formulario

--- adedu-app/src/app/app.module.ts
+++ adedu-app/src/app/app.module.ts
@@ -8,9 +8,19 @@ import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, CrearUsuarioComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CrearUsuarioComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -18,6 +28,10 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
